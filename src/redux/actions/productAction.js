9e663463a840d8067fc91d5240996094fa179f57
favianhanasta <favianhanastap@gmail.com>
2022-03-31 11:00:53 +0700

    PHAR-11-favian : Aktivasi fitur filter by name dan atau category

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -2,10 +2,29 @@ import axios from 'axios';
 import { API_URL } from '../../helper';
 
 
-export const getProduct = ()=>{
+export const getProduct = (search=null)=>{
     return async(dispatch)=>{
         try{
-            let res = await axios.get(`${API_URL}/product`);
+            let res;
+            if(search){
+                if(search.nama){
+                    if(search.category>0){
+                        res = await axios.get(`${API_URL}/product?idcategory=${search.category}&nama=${search.nama}`);    
+                    }
+                    else{
+                        res = await axios.get(`${API_URL}/product?nama=${search.nama}`);
+                    }
+                }else if(search.category>0){
+                    if(search.nama){
+                        res = await axios.get(`${API_URL}/product?idcategory=${search.category}&nama=${search.nama}`);  
+                    }else{
+                        res = await axios.get(`${API_URL}/product?idcategory=${search.category}`);
+                    }
+                }
+            }else{
+                res = await axios.get(`${API_URL}/product`);
+            }
+            console.log("search",search)
             dispatch({
                 type: 'GET_PRODUCT',
                 payload : res.data.dataProduct
