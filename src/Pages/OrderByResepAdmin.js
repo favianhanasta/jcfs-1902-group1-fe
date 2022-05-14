ed355777fc2b61f56045371e0788398ffef358ee
favianhanasta <favianhanastap@gmail.com>
2022-05-14 17:30:39 +0800

    PHAR-61-favian : fix flow order by resep

--- src/Pages/OrderByResepAdmin.js
+++ src/Pages/OrderByResepAdmin.js
@@ -3,14 +3,15 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Badge, Button } from 'reactstrap';
 import { API_URL } from '../helper';
-import {getTransactionByResep} from '../redux/actions'
+import { getTransactionByResep } from '../redux/actions';
+const cartempty = require('../Assets/empty.png');
 
 class OrderByResepAdmin extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    componentDidMount(){
+    componentDidMount() {
         this.props.getTransactionByResep()
     }
     printTransaksi = () => {
@@ -19,7 +20,7 @@ class OrderByResepAdmin extends React.Component {
                 <div className='col-5 my-4 mx-4 transaksi-box row' key={idx}>
                     <div className='col-10'>
                         <p className='font-price clr-orange2' style={{ fontWeight: 'bold' }}>{val.invoice}</p>
-                        <p className='clr-blue' style={{fontWeight:'600'}}>User : {val.username}</p>
+                        <p className='clr-blue' style={{ fontWeight: '600' }}>User : {val.username}</p>
                     </div>
                     <div className='col-2'>
                         <Badge className='p-1 float-right'
@@ -28,7 +29,7 @@ class OrderByResepAdmin extends React.Component {
                         </Badge>
                     </div>
                     <div className='col-12'>
-                    <img src={API_URL + val.url} alt="" width='100%' />
+                        <img src={API_URL + val.url} alt="" width='100%' />
                     </div>
                     <div className='my-2 col-12'>
                         <Link to={`/custom-order?idorder=${val.idorder}`}>
@@ -39,29 +40,39 @@ class OrderByResepAdmin extends React.Component {
             )
         })
     }
-    render() { 
-        console.log('s',this.props.byResep)
-        return ( 
-            <div className='container' style={{marginTop:'1%',height:'100vh'}}>
+    render() {
+        console.log('s', this.props.byResep)
+        return (
+            <div className='container' style={{ marginTop: '1%', minHeight: '100%' }}>
                 <div className='row'>
                     <div className='col-md-6 d-flex py-1'>
                         <h5 className='clr-blue'>Halaman Admin</h5>
                         <h5 className='mx-3'>|</h5>
-                        <h5 className='clr-orange2'>Manajemen Order By Resep</h5>
+                        <h5 className='clr-orange2'>Waiting List Order By Resep</h5>
                     </div>
                 </div>
-                <div className='row'>
-                    {this.printTransaksi()}
-                </div>
+                {
+                    this.props.byResep.length > 0 ?
+                        <div className='row'>
+                            {this.printTransaksi()}
+                        </div>
+                        :
+                        <div className='text-center transaksi-box' style={{ padding: '10%' }}>
+                            <div className='d-flex justify-content-center'>
+                                <img src={cartempty} />
+                            </div>
+                            <h1 className='clr-orange'>Belum Ada Order dari user</h1>
+                        </div>
+                }
             </div>
-         );
+        );
     }
 }
 
-const mapToProps =(state)=>{
-    return{
-        byResep : state.transactionReducer.byResep
+const mapToProps = (state) => {
+    return {
+        byResep: state.transactionReducer.byResep
     }
 }
 
-export default connect(mapToProps,{getTransactionByResep})(OrderByResepAdmin);
+export default connect(mapToProps, { getTransactionByResep })(OrderByResepAdmin);
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
-//export const API_URL="https://localhost:2021"
+// export const API_URL="https://localhost:2021"
 
 export const API_URL="https://jcfs190201api.purwadhikabootcamp.com"
